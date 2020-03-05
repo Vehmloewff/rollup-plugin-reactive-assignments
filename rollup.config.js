7cc6b6339517bdfad840a88ed93acf48bee43b2c
Elijah Mooring <ejsisgreat@gmail.com>
2020-03-04 19:15:08 -0600

    Make reactive-assignments an external dependency

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,10 @@ import globFiles from 'rollup-plugin-glob-files';
 import command from 'rollup-plugin-command';
 
 import pkg from './package.json';
+import { isAbsolute } from 'path';
 
 const name = 'todo';
-const sourcemap = true;
+const sourcemap = false;
 const prod = process.env.NODE_ENV === 'production';
 const watching = process.env.ROLLUP_WATCH;
 
@@ -23,7 +24,7 @@ if (prod) output.push({ file: pkg.module, format: 'es', ...sharedOutputOptions }
 export default {
 	input: prod ? 'src/index.ts' : '@tests',
 	output,
-	external: [`reactive-assignments`, `path`, `util`],
+	external: id => id[0] !== '.' && !isAbsolute(id),
 	plugins: [
 		globFiles({
 			key: `@tests`,
